194c31cd78bfdb654d750b718413302789e76175
Aakash <aaka2005@gmail.com>
2025-03-13 17:12:28 -0400

    Added more abstractions

--- src/main/java/ca/mcmaster/se2aa4/island/team31/Direction.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team31/Direction.java
@@ -1,6 +1,7 @@
 package ca.mcmaster.se2aa4.island.team31;
 
 public class Direction {
+    //drone can only move in these directions
     public enum CardinalDirection {
         NORTH,
         EAST,
