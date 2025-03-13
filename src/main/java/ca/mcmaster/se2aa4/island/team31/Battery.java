c4a388004997743647d8de905e14b9aecd723c0a
Aakash <aaka2005@gmail.com>
2025-03-13 17:58:55 -0400

    Added battery functionality

--- src/main/java/ca/mcmaster/se2aa4/island/team31/Battery.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team31/Battery.java
@@ -2,18 +2,28 @@ package ca.mcmaster.se2aa4.island.team31;
 
 public class Battery {
     private int batteryLevel;
+    //private final int initialBatteryLevel;
 
-    public Battery(int batteryLevel) {
-        if (batteryLevel < 0 || batteryLevel > 100) {
-            //battery level should be valid between 0 and 100
-            throw new IllegalArgumentException("Battery level must be between 0 and 100");
+    public Battery(int initialBatteryLevel) {
+        //this.initialBatteryLevel = initialBatteryLevel;
+        this.batteryLevel = initialBatteryLevel;
+    }
+
+    public int useBattery(int batteryLevel) {
+        if(batteryLevel > this.batteryLevel) {
+            throw new IllegalArgumentException("Battery level cannot be negative");
         }
-        this.batteryLevel = batteryLevel;
+        this.batteryLevel -= batteryLevel;
+        return this.batteryLevel;
     }
 
     public int getBatteryLevel() {
         return batteryLevel;
     }
-    
+
+    public boolean goHome(int x, int y, int costPerMove){
+        int costToGoHome = (x + y) * costPerMove;
+        return batteryLevel <= costToGoHome;
+    }
     
 }
