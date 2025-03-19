f8e0f8cf61dd7e8b1dbdabe827f2e5f5a84fa819
saqib-21 <saqibmominkhan@gmail.com>
2025-03-18 20:57:12 -0400

    simple dronecontroller (close #8)

--- src/main/java/ca/mcmaster/se2aa4/island/team31/DroneController.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team31/DroneController.java
@@ -3,29 +3,108 @@ package ca.mcmaster.se2aa4.island.team31;
 //so this class will be responsible for controlling the drone
 import java.util.LinkedList;
 import java.util.Queue;
+
 import org.json.JSONObject;
 
+import ca.mcmaster.se2aa4.island.team31.Direction.CardinalDirection;
 import ca.mcmaster.se2aa4.island.team31.Drone.Battery;
 
 public class DroneController{
-    private Direction direction;
-    int level;
+    private Direction.CardinalDirection direction;
+    private int batteryLevel;
     int x;
     int y;
+    private Queue<JSONObject> actionQueue;
+    private Battery battery;
 
-    public DroneController(Integer level, String start){
-        this.level = level;
+
+
+    public DroneController(Integer batteryLevel, String startDirection){
+        this.battery = new Battery(batteryLevel);
+        this.actionQueue = new LinkedList<>();
+        this.x = 0; // Assuming initial position is (0,0), can be changed dynamically
+        this.y = 0;
         try{
-            this.direction = Direction.valueOf(start);
+            this.direction = Direction.CardinalDirection.valueOf(startDirection);
         } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException("Invalid direction: " + Starting);
+            throw new IllegalArgumentException("Invalid direction: " + startDirection);
 
         }
     }
 
     public void addAction(JSONObject decision) {
+            actionQueue.add(decision);
+            updateBattery(decision);
+        }
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addAction'");
+        //throw new UnsupportedOperationException("Unimplemented method 'addAction'");
+
+    
+
+        private void updateBattery(JSONObject decision) {
+            String action = decision.getString("action");
+            int cost = 0;
+    
+            if (action.equals("fly")) {
+                cost = 1;
+                moveForward();
+            } else if (action.equals("heading")) {
+                cost = 1;
+                changeDirection(decision.getString("direction"));
+            } else if (action.equals("scan")) {
+                cost = 2;
+            } else if (action.equals("echo")) {
+                cost = 1;
+            } else if (!action.equals("stop")) {
+                throw new IllegalArgumentException("Unknown action: " + action);
+            }
+    
+            battery.useBattery(cost);
+        }
+    
+        private void moveForward() {
+            if (direction == CardinalDirection.N) {
+                y -= 1;
+            } else if (direction == CardinalDirection.S) {
+                y += 1;
+            } else if (direction == CardinalDirection.E) {
+                x += 1;
+            } else if (direction == CardinalDirection.W) {
+                x -= 1;
+            }
+        }
+    
+        //will later break into its own class
+        private void changeDirection(String newDirection) {
+            try {
+                this.direction = CardinalDirection.valueOf(newDirection);
+            } catch (IllegalArgumentException e) {
+                throw new IllegalArgumentException("Invalid heading direction: " + newDirection);
+            }
+        }
+    
+        public boolean shouldReturnHome() {
+            return battery.goHome(x, y, 1);
+        }
+    
+        public int getBatteryLevel() {
+            return battery.getBatteryLevel();
+        }
+    
+        public CardinalDirection getDirection() {
+            return direction;
+        }
+    
+        public int getX() {
+            return x;
+        }
+    
+        public int getY() {
+            return y;
+        }
+    
+        public JSONObject getNextAction() {
+            return actionQueue.poll();
+        }
     }
     
-}
