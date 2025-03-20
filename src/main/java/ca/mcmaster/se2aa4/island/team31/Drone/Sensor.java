5e360010c4c7f2f0102d267bb0b8dca302f9d1e8
Aakash <aaka2005@gmail.com>
2025-03-20 18:06:55 -0400

    feat: Add Sensor class for drone navigation and echo-based sensing (close #13)

---
+++ src/main/java/ca/mcmaster/se2aa4/island/team31/Drone/Sensor.java
@@ -0,0 +1,37 @@
+package ca.mcmaster.se2aa4.island.team31.Drone;
+
+import ca.mcmaster.Interface.ExplorerDrone;
+import ca.mcmaster.se2aa4.island.team31.Enums.Direction;
+
+public class Sensor extends ExplorerDrone {
+    private Gps gps = new Gps();
+    private DroneActions droneActions = new DroneActions();
+    private Direction.CardinalDirection direction;
+
+    public Sensor(Direction.CardinalDirection direction) {
+        this.direction = direction;
+    }
+
+    public void echoForward(){
+        update(droneActions.echo(this.direction));
+    }
+
+    public void echoRight(){
+        update(droneActions.echo(gps.getRight(this.direction)));
+    }
+
+    public void echoLeft(){
+        update(droneActions.echo(gps.getLeft(this.direction)));
+    }
+
+    //changes to scan different directions
+    public void setHeading(Direction.CardinalDirection direction){
+        this.direction = direction;
+    }
+
+    public void scan(){
+        update(droneActions.scan());
+    }
+    
+    
+}
--- src/main/java/ca/mcmaster/se2aa4/island/team31/Sensor.java
+++
@@ -1,6 +0,0 @@
-package ca.mcmaster.se2aa4.island.team31;
-
-public class Sensor {
-    public Sensor() {
-    }
-}
