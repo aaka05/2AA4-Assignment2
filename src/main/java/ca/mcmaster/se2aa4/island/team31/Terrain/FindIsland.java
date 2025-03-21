5d4b484807d9eda6259e474d550d5284a5aa0e7d
Aakash <aaka2005@gmail.com>
2025-03-20 23:38:24 -0400

    Abstraction for OnIsland search

--- src/main/java/ca/mcmaster/se2aa4/island/team31/Terrain/FindIsland.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team31/Terrain/FindIsland.java
@@ -22,27 +22,32 @@ public class FindIsland extends State {
 
     @Override
     public State getNextState(JSONObject command) {
+        // first check if we found the island
         if (landDetector.foundGround(command)) {
-            drone.stop();
-    
+            // return new OnIsland(drone, sensor);
         }
         
-        if(echo){
+        // do our zigzag pattern
+        if(echo) {
+            // check what's in front of us
             sensor.echoForward();
             echo = false;
         }
-        else if(turnRight){
-            drone.turnRight();
+        else if(turnRight) {
+            // when we turn right, we move diagonally down-right
+            drone.turnRight();  // this actually moves us diagonally
             turnRight = false;
             turnLeft = true;
-            echo = true;
+            echo = true;        // echo after each turn
         }
-        else if(turnLeft){
-            drone.turnLeft();
+        else if(turnLeft) {
+            // when we turn left, we move diagonally down-left
+            drone.turnLeft();   // this actually moves us diagonally
             turnLeft = false;
             turnRight = true;
-            echo = true;
+            echo = true;        // echo after each turn
         }
+        
         return this;
     }
 
